0698777bf29c2f7ff08ece20d6346068b6a4ff48
K. Pramadu Shehan Chithakshana <151552571+Pramadu2001@users.noreply.github.com>
2024-07-01 21:56:21 +0530

    4th

--- EC/app/url.py
+++ EC/app/url.py
@@ -2,11 +2,21 @@ from django.urls import path
 from .import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_view
+from .forms import LoginForm
 
 urlpatterns =[
-    path("",views.home),
+    path("",views.home ,name ="home"),
+    path("About/",views.About ,name ="About"),
+    path("Contact/",views.Contact ,name ="Contact"),
+    path("Promotion/",views.Promotion ,name ="Promotion"),
     path("category/<slug:val>/",views.CategoryView.as_view(),name ="category"),
-     path("product-details/<int:pk>",views.ProductDetails.as_view(),name ="product-details"),
+    path("category-title/<val>",views.CategoryView.as_view(),name ="category-title"),
+    path("product-details/<int:pk>",views.ProductDetails.as_view(),name ="product-details"),
+
+    #loging authentication
+      path("registration/",views.CustomRegistrationView.as_view(),name ="customerregistration"),
+      path("account/login/", auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name="Login")
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- EC/app/views.py
+++ EC/app/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from urllib import request
+from .forms import CustomerRegistrationForm
 from .models import products
 from django.db.models import Count 
+from django.contrib import messages
 
 
 def home(request):
     return render(request, "app/home.html")
-# Create your views here.
+
+def About(request):
+    return render(request, "app/about.html")
+
+def Contact(request):
+    return render(request, "app/contact.html")
+
+def Promotion(request):
+    return render(request, "app/promotion.html")
+
 
 class CategoryView(View):
     def get(self,request,val):
@@ -18,5 +29,19 @@ class CategoryView(View):
 
 class ProductDetails(View):
     def get(self, request, pk):
-        product = products.objects.get(pk=pk)  # Ensure the model name starts with a lowercase 'p'
+        product = products.objects.get(pk=pk)  
         return render(request, "app/productdetails.html", locals())
+    
+class CustomRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html',locals())
+    
+    def post(self,request):
+         form = CustomerRegistrationForm( request.POST)
+         if form.is_valid():
+             form.save() 
+             messages.success(request,"congralulations!Registerd Sucessfully! ")
+         else:
+            messages.warning(request,"Invalid data!!")
+         return render(request, 'app/customerregistration.html',locals())
