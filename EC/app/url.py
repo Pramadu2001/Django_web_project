3e636b0fea53e03ce48369da6235a53ff4f7306e
K. Pramadu Shehan Chithakshana <151552571+Pramadu2001@users.noreply.github.com>
2024-07-21 22:08:46 +0530

    5th

--- EC/app/url.py
+++ EC/app/url.py
@@ -3,7 +3,7 @@ from .import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_view
-from .forms import LoginForm, MyPasswordChangeForm, MyPasswordResetForm
+from .forms import LoginForm, MyPasswordChangeForm, MyPasswordResetForm, MySetPasswordForm
 
 urlpatterns =[
     path("",views.home ,name ="home"),
@@ -13,14 +13,13 @@ urlpatterns =[
     path("category/<slug:val>/",views.CategoryView.as_view(),name ="category"),
     path("category-title/<val>",views.CategoryView.as_view(),name ="category-title"),
     path("product-details/<int:pk>",views.ProductDetails.as_view(),name ="product-details"),
-    path("profile/", views.profileView.as_view(),name="profile"),
+    path("accounts/profile/", views.profileView.as_view(),name="profile"),
     path("address/", views.address,name="address"),
     path("updateAddress/<int:pk>", views.updateaddress.as_view(),name="updateAddress"),
 
     #loging authentication
       path("registration/",views.CustomRegistrationView.as_view(),name ="customerregistration"),
       path("account/login/", auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name="Login"),
-      path("password-reset/", auth_view.PasswordResetView.as_view(template_name='app/reset.html', form_class = MyPasswordResetForm), name="password-reset"),
       path("passwordchange/", auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class = MyPasswordChangeForm, success_url='/passwordchangedone' ), name="passwordchange"),
       path("passwordchangedone/", auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html' ), name="passwordchangedone"),
       path("logout/", auth_view.LogoutView.as_view(next_page='Login'), name="logout"),
